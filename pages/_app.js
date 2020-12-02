1e39b42461b7062a3654cf2bf1b1af040887e076
MAlngaawy <m.alngaawy@gmail.com>
2020-12-02 17:33:02 +0200

    add the packages and implement the SPA in next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+// import { AppProps } from 'next/app';
+// import React from 'react'
+
+function App({ Component, pageProps }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : <Component {...pageProps} />}
+    </div>
+  );
 }
 
-export default MyApp
+export default App;
